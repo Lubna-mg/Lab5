b9e73b3dace4d583dbf9e2aec8acb2061bbc3b21
Lubna-mg <lubna.mg@hotmail.com>
2025-03-10 22:30:43 +0300

    First commit - Upload Lab5 project

---
+++ apps/bookmodule/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="bookmodules.index"),
+    path('html5/links/', views.html5_links, name="bookmodules.html5_links"),
+    path('html5/text/formatting/', views.text_formatting, name="bookmodules.text_formatting"),
+    path('html5/listing/', views.listing, name="bookmodules.listing"),
+    path('html5/tables/', views.tables, name="bookmodules.tables"),
+]
---
+++ apps/bookmodule/views.py
@@ -0,0 +1,16 @@
+﻿from django.shortcuts import render
+
+def index(request):
+    return render(request, 'bookmodules/index.html')
+
+def html5_links(request):
+    return render(request, 'bookmodules/html5_links.html')
+
+def text_formatting(request):
+    return render(request, 'bookmodules/text_formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodules/listing.html')
+
+def tables(request):
+    return render(request, 'bookmodules/tables.html')
